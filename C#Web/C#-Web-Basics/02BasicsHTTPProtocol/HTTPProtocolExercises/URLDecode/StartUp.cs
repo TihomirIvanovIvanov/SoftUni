00d53dc51be2ce291f62469515865a5d4a1c94f0
TihomirIvanovIvanov <tihomir_2005@abv.bg>
2019-04-10 11:33:48 +0300

    add HTTPProtocolExercises

---
+++ C#Web/C#-Web-Basics/02BasicsHTTPProtocol/HTTPProtocolExercises/URLDecode/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace URLDecode
+{
+    using System;
+    using System.Net;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(WebUtility.UrlDecode(input));
+        }
+    }
+}

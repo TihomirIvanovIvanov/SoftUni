1e5f64f5b1ebf78d9d6bbf5df5a5b38527c31cf0
TihomirIvanovIvanov <tihomir_2005@abv.bg>
2019-04-11 20:50:12 +0300

    add Interfaces And Abstraction lab tasks

---
+++ C#Fundamentals/C#OOP-Basics/05InterfacesAndAbstraction/InterfacesLab1/Cars/Tesla.cs
@@ -0,0 +1,18 @@
+﻿namespace Cars
+{
+    public class Tesla : Car, ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int batteries)
+            : base(model, color)
+        {
+            this.Batteries = batteries;
+        }
+
+        public int Batteries { get; private set; }
+
+        public override string GetCarInfo()
+        {
+            return base.GetCarInfo() + $" with {this.Batteries} Batteries";
+        }
+    }
+}
---
+++ C#Fundamentals/C#OOP-Basics/05InterfacesAndAbstraction/InterfacesLab1/Shapes/Circle.cs
@@ -0,0 +1,46 @@
+﻿namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get => this.radius;
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public void Draw()
+        {
+            var radiusIn = this.Radius - 0.4;
+            var radiusOut = this.Radius + 0.4;
+
+            for (double y = this.Radius; y >= -this.Radius - 1; --y)
+            {
+                for (double x = -this.Radius; x < radiusOut; x += 0.5)
+                {
+                    var value = x * x + y * y;
+
+                    if (value >= radiusIn * radiusIn && value <= radiusOut * radiusOut)
+                    {
+                        System.Console.Write("*");
+                    }
+                    else
+                    {
+                        System.Console.Write(" ");
+                    }
+                }
+
+                System.Console.WriteLine();
+            }
+        }
+    }
+}

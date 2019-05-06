d84e00ad23bc389bda67e9295f272fa68abdd6b1
TihomirIvanovIvanov <tihomir_2005@abv.bg>
2019-05-06 19:13:44 +0300

    add DependencyInversion exercise task

---
+++ C#Fundamentals/C#OOP-Advanced/06CommunicationAndEvents/CommunicationAndEventsExer/DependencyInversion/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using P03_DependencyInversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        PrimitiveCalculator calculator = new PrimitiveCalculator(new AdditionStrategy());
+
+        string[] input = Console.ReadLine().Split();
+        while (input[0] != "End")
+        {
+            if (input[0].Equals("mode"))
+            {
+                calculator.ChangeStrategy(char.Parse(input[1]));
+            }
+            else
+            {
+                var firstNum = int.Parse(input[0]);
+                var secondNum = int.Parse(input[1]);
+
+                Console.WriteLine(calculator.PerformCalculation(firstNum, secondNum));
+            }
+
+            input = Console.ReadLine().Split();
+        }
+    }
+}

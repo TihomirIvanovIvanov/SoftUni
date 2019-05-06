e07d7e601f63bd52cca991deca6b6f98090fa041
TihomirIvanovIvanov <tihomir_2005@abv.bg>
2019-05-06 19:15:53 +0300

    add KingsGambitExtended exercise task

---
+++ C#Fundamentals/C#OOP-Advanced/06CommunicationAndEvents/CommunicationAndEventsExer/KingsGambitExtended/Models/RoyalGuard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class RoyalGuard : Soldier
+{
+    private const int DefaultHits = 3;
+
+    public RoyalGuard(string name, King kingToDefend) 
+        : base(name, DefaultHits, kingToDefend)
+    {
+    }
+
+    public override void OnKingBeingAttacked(object sender, EventArgs e)
+    {
+        Console.WriteLine($"Royal Guard { this.Name } is defending!");
+    }
+}
---
+++ C#Fundamentals/C#OOP-Advanced/06CommunicationAndEvents/CommunicationAndEventsExer/KingsGambitExtended/Models/Soldier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public abstract class Soldier : INameable
+{
+    private readonly King kingDefended;
+
+    private int hitsTaken;
+
+    public Soldier(string name, int hitsTaken, King kingToDefend)
+    {
+        this.Name = name;
+        this.hitsTaken = hitsTaken;
+        this.kingDefended = kingToDefend;
+    }
+
+    public event EventHandler<KillEventArgs> SoldierKilled;
+
+    public string Name { get; }
+
+    public abstract void OnKingBeingAttacked(object sender, EventArgs e);
+
+    public void TakeAttack()
+    {
+        this.hitsTaken--;
+
+        if (this.hitsTaken <= 0)
+        {
+            this.OnSoldierKilled();
+        }
+    }
+
+    private void OnSoldierKilled()
+    {
+        this.SoldierKilled?.Invoke(this, new KillEventArgs(this, this.kingDefended));
+    }
+}

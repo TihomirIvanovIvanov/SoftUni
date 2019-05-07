146012605b4bf8ff80192d8c6b1c3bff9bd990fd
TihomirIvanovIvanov <tihomir_2005@abv.bg>
2019-05-07 19:19:02 +0300

    add Minedraft Unit Testing project

---
+++ C#Fundamentals/C#OOP-Advanced/ExamPreperationFrom07092017MineDraft/Minedraft.Tests/ProviderControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+[TestFixture]
+public class ProviderControllerTests
+{
+    private IEnergyRepository energyRepository;
+    private IProviderController providerController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.energyRepository = new EnergyRepository();
+        this.providerController = new ProviderController(this.energyRepository);
+    }
+
+    [Test]
+    public void ProviderController_RegisterProvider_Valid()
+    {
+        //Arrange 
+        var args = "Pressure 40 100".Split().ToList();
+        this.providerController.Register(args);
+
+        //Act
+        var countOfProviders = this.providerController.Entities.Count();
+
+        //Assert
+        Assert.AreEqual(1, countOfProviders, "Count of registered providers is not correct!");
+    }
+
+    [Test]
+    public void ProviderController_ProduceMethod_Removes_Broken_Provider()
+    {
+        var args = "Pressure 40 100".Split().ToList();
+        var args1 = "Solar 80 100".Split().ToList();
+
+        this.providerController.Register(args);
+        this.providerController.Register(args1);
+        this.providerController.Produce();
+
+        var actualResult = this.providerController.TotalEnergyProduced;
+
+        Assert.AreEqual(300, actualResult, "Total energy produce is not correct!");
+    }
+
+    [Test]
+    public void ProviderController_RepairProvider_Valid()
+    {
+        var args = "Pressure 40 100".Split().ToList();
+        var args1 = "Solar 80 100".Split().ToList();
+        var args2 = "Standart 10 100".Split().ToList();
+
+        this.providerController.Register(args);
+        this.providerController.Register(args1);
+        this.providerController.Register(args2);
+
+        for (int i = 0; i <= 10; i++)
+        {
+            this.providerController.Produce();
+        }
+
+        var actualAliveProvidersCount = this.providerController.Entities.Count;
+
+        Assert.AreEqual(1, actualAliveProvidersCount, "Alive providers count is not correct!");
+    }
+}

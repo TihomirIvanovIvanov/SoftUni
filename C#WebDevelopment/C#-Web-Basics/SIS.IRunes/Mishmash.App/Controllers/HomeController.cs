0c479dc6144bac17bc169a46819fd89b88749ca6
TihomirIvanovIvanov <tihomir_2005@abv.bg>
2019-06-08 16:08:27 +0300

    Implemented the home page

---
+++ C#WebDevelopment/C#-Web-Basics/SIS.IRunes/Mishmash.App/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿namespace Mishmash.App.Controllers
+{
+    using SIS.HTTP.Common;
+    using SIS.MvcFramework;
+    using SIS.MvcFramework.Attributes.Http;
+    using SIS.MvcFramework.Result;
+    using System.Collections.Generic;
+
+    public class HomeController : Controller
+    {
+        [HttpGet(Url = GlobalConstants.HomeRedirectPath)]
+        public ActionResult IndexSlash()
+        {
+            return Index();
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Test(IEnumerable<string> list)
+        {
+            return this.View();
+        }
+    }
+}

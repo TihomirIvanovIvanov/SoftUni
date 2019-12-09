3bd50c424f7cdf561a9ddea8d2c57ac741fc10eb
tihomir <tihomir_2005@abv.bg>
2019-12-09 15:31:03 +0200

    Added IHttpRequest and HttpRequest

---
+++ C#WebDevelopment/C#-Web-Basics/01HTTPProtocol/SIS/SIS.HTTP/Requests/Contracts/IHttpRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace SIS.HTTP.Requests.Contracts
+{
+    using Enums;
+    using Headers.Contracts;
+    using System.Collections.Generic;
+
+    public interface IHttpRequest
+    {
+        string Path { get; }
+
+        string Url { get; }
+
+        Dictionary<string, object> FormData { get; }
+
+        Dictionary<string, object> QueryData { get; }
+
+        IHttpHeaderCollection Headers { get; }
+
+        HttpRequestMethod RequestMethod { get; }
+    }
+}

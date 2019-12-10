269d68b89a5b4429efb37f1a09593a2c78ef5940
tihomir <tihomir_2005@abv.bg>
2019-12-10 12:08:16 +0200

    Added IServerRoutingTable.cs and ServerRoutingTable.cs

---
+++ C#WebDevelopment/C#-Web-Basics/01HTTPProtocol/SIS/SIS.WebServer/Routing/ServerRoutingTable.cs
@@ -0,0 +1,51 @@
+﻿namespace SIS.WebServer.Routing
+{
+    using Contracts;
+    using HTTP.Common;
+    using HTTP.Enums;
+    using HTTP.Requests.Contracts;
+    using HTTP.Responses.Contracts;
+    using System;
+    using System.Collections.Generic;
+
+    public class ServerRoutingTable : IServerRoutingTable
+    {
+        private readonly Dictionary<HttpRequestMethod, Dictionary<string, Func<IHttpRequest, IHttpResponse>>> routes;
+
+        public ServerRoutingTable()
+        {
+            this.routes = new Dictionary<HttpRequestMethod, Dictionary<string, Func<IHttpRequest, IHttpResponse>>>
+            {
+                [HttpRequestMethod.Get] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+                [HttpRequestMethod.Post] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+                [HttpRequestMethod.Put] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+                [HttpRequestMethod.Delete] = new Dictionary<string, Func<IHttpRequest, IHttpResponse>>(),
+            };
+        }
+
+        public void Add(HttpRequestMethod method, string path, Func<IHttpRequest, IHttpResponse> func)
+        {
+            CoreValidator.ThrowIfNull(method, nameof(method));
+            CoreValidator.ThrowIfNullOrEmpty(path, nameof(path));
+            CoreValidator.ThrowIfNull(func, nameof(func));
+
+            this.routes[method].Add(path, func);
+        }
+
+        public bool Contains(HttpRequestMethod method, string path)
+        {
+            CoreValidator.ThrowIfNull(method, nameof(method));
+            CoreValidator.ThrowIfNullOrEmpty(path, nameof(path));
+
+            return this.routes.ContainsKey(method) && this.routes[method].ContainsKey(path);
+        }
+
+        public Func<IHttpRequest, IHttpResponse> Get(HttpRequestMethod requestMethod, string path)
+        {
+            CoreValidator.ThrowIfNull(requestMethod, nameof(requestMethod));
+            CoreValidator.ThrowIfNullOrEmpty(path, nameof(path));
+
+            return this.routes[requestMethod][path];
+        }
+    }
+}

38979284ec97d44dfc2de4a9023978bfddd27370
tihomir <tihomir_2005@abv.bg>
2019-12-12 21:49:16 +0200

    Added State Management lecture and exercises

--- C#WebDevelopment/C#-Web-Basics/03AsynchronousProcessing/SIS.SoftUniInformationServices/SIS.Http/Extentions/HttpResponseStatusExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SIS.Http.Extentions
-{
-    using System.Net;
-
-    public static class HttpResponseStatusExtensions
-    {
-        public static string GetResponseLine(this HttpStatusCode statusCode) => $"{(int)statusCode} {statusCode}";
-    }
-}

862bece6ba3b44d371d4953e61a2bf933e4994b6
tihomir <tihomir_2005@abv.bg>
2019-12-14 16:33:03 +0200

    Implemented Database layer /DbContext and Models/

---
+++ C#WebDevelopment/C#-Web-Basics/01HTTPProtocol/SIS/Demo.Data/DemoDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace Demo.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class DemoDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DemoDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=DemoDb;Integrated Security=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(user => user.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

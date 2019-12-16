b80246442bcf0b6c6b59ca20e9da8cedb7050bcd
tihomir <tihomir_2005@abv.bg>
2019-12-16 16:37:33 +0200

    Implemented Index and Home response in HomeController

--- C#WebDevelopment/C#-Web-Basics/01HTTPProtocol/SIS/SIS.Demo/Controllers/HomeController.cs
+++ C#WebDevelopment/C#-Web-Basics/01HTTPProtocol/SIS/SIS.Demo/Controllers/HomeController.cs
@@ -5,8 +5,26 @@
 
     public class HomeController : BaseController
     {
+        public HomeController(IHttpRequest httpRequest)
+        {
+            this.HttpRequest = httpRequest;
+        }
+
+        public IHttpRequest HttpRequest { get; set; }
+
+        public IHttpResponse Index(IHttpRequest httpRequest)
+        {
+            return this.View();
+        }
+
         public IHttpResponse Home(IHttpRequest httpRequest)
         {
+            if (!this.IsLoggedIn())
+            {
+                return this.Redirect("/users/login");
+            }
+
+            this.ViewData["Username"] = this.HttpRequest.Session.GetParameter("username");
             return this.View();
         }
     }

5bd922a50cbfb72c3340b93d0e53d90a63de0e49
Tihomir Ivanov <tihomir_2005@abv.bg>
2019-12-18 17:04:23 +0200

    Implemented database layer

---
+++ C#WebDevelopment/C#-Web-Basics/04WorkshopBootstrap/IRunesSkeleton/IRunes.Data/DatabaseConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace IRunes.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString = @"Server=.;Database=IRunesDB;Integrated Security=True;";
+    }
+}

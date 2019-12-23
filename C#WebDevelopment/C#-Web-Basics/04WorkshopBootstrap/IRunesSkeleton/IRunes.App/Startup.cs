bfecf88da2183cc482b34d5fa9dc93f10b12db0f
Tihomir Ivanov <tihomir_2005@abv.bg>
2019-12-23 23:08:22 +0200

    Introduced WebHost to IRunes app

---
+++ C#WebDevelopment/C#-Web-Basics/04WorkshopBootstrap/IRunesSkeleton/IRunes.App/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace IRunes.App
+{
+    using Data;
+    using SIS.WebServer;
+    using SIS.WebServer.Routing;
+
+    public class Startup : IMvcApplication
+    {
+        public void Configure(IServerRoutingTable serverRoutingTable)
+        {
+            using (var context = new RunesDbContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void ConfigureServices()
+        {
+        }
+    }
+}
---
+++ C#WebDevelopment/C#-Web-Basics/04WorkshopBootstrap/IRunesSkeleton/SIS.WebServer/WebHost.cs
@@ -0,0 +1,24 @@
+﻿using SIS.WebServer.Routing;
+using System;
+
+namespace SIS.WebServer
+{
+    public static class WebHost
+    {
+        public static void Start(IMvcApplication application)
+        {
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+            AutoRegisterRoutes(application, serverRoutingTable);
+            application.ConfigureServices();
+            application.Configure(serverRoutingTable);
+
+            var server = new Server(8000, serverRoutingTable);
+            server.Run();
+        }
+
+        private static void AutoRegisterRoutes(IMvcApplication application, IServerRoutingTable serverRoutingTable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

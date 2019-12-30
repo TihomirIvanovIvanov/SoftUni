1c03d7013a8951066ebcab9ae78cab74a3c278ae
Tihomir Ivanov <tihomir_2005@abv.bg>
2019-12-30 12:23:32 +0200

    Implemented dependency injection into MVC Framework

---
+++ C#WebDevelopment/C#-Web-Basics/04WorkshopBootstrap/SIS/SIS.WebServer/DependencyContainer/IServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SIS.MvcFramework.DependencyContainer
+{
+    public interface IServiceProvider
+    {
+        void Add<TSourse, TDestination>()
+            where TDestination : TSourse;
+
+        object CreateInstance(Type type);
+    }
+}

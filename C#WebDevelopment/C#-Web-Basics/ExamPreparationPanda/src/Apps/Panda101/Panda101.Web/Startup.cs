083be8702ee063fad3b224fdc124840da66ac5dc
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-01-27 11:08:30 +0200

    Panda101 architecture created

---
+++ C#WebDevelopment/C#-Web-Basics/ExamPreparationPanda/src/Apps/Panda101/Panda101.Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using SIS.MvcFramework;
+using SIS.MvcFramework.DependencyContainer;
+using SIS.MvcFramework.Routing;
+
+namespace Panda101.Web
+{
+    internal class Startup : IMvcApplication
+    {
+        public void Configure(IServerRoutingTable serverRoutingTable)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ConfigureServices(IServiceProvider serviceProvider)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

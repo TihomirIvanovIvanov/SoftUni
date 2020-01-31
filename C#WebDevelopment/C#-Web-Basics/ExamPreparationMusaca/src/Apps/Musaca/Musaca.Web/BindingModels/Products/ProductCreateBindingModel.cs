b69dc22be1a98c84d4373c1aa0fe80ef80db9ce0
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-01-31 16:11:06 +0200

    Implemented Products/Create, Products/All views and binding models

---
+++ C#WebDevelopment/C#-Web-Basics/ExamPreparationMusaca/src/Apps/Musaca/Musaca.Web/BindingModels/Products/ProductCreateBindingModel.cs
@@ -0,0 +1,20 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+
+namespace Musaca.Web.BindingModels.Products
+{
+    public class ProductCreateBindingModel
+    {
+        private const string NameErrorMessage = "Invalid product name length! Must be between 5 and 20 symbols!";
+
+        private const string PriceErrorMessage = "Invalid product price length! Must be greater than or equals to 0.01!";
+
+        [RequiredSis]
+        [StringLengthSis(5, 20, NameErrorMessage)]
+        public string Name { get; set; }
+
+        [RequiredSis]
+        [RangeSis(0.01, 1.7976931348623157E+308, PriceErrorMessage)]
+        public double Price { get; set; }
+
+    }
+}
---
+++ C#WebDevelopment/C#-Web-Basics/ExamPreparationMusaca/src/Apps/Musaca/Musaca.Web/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Musaca.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Result;
+using SIS.MvcFramework.Mapping;
+using Musaca.Web.ViewModels.Products;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using Musaca.Web.BindingModels.Products;
+using Musaca.Models;
+using System.Linq;
+
+namespace Musaca.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [Authorize]
+        public IActionResult All()
+        {
+            var model = this.productService.GetAll().To<ProductsAllViewModel>().ToList();
+
+            return this.View(model);
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create(ProductCreateBindingModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+            var product = model.To<Product>();
+            this.productService.CreateProduct(product);
+
+            return this.Redirect("/Products/All");
+        }
+    }
+}

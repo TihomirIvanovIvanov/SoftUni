9df02a7699215f29894007b7360c0eb6dc1cbfd9
Tihomir Ivanov <tihomir_2005@abv.bg>
2020-02-12 14:10:18 +0200

    Implement submission create page

--- C#WebDevelopment/C#-Web-Basics/ExamPreparationSULSJan2020/src/SulsApp/Controllers/SubmissionsController.cs
+++ C#WebDevelopment/C#-Web-Basics/ExamPreparationSULSJan2020/src/SulsApp/Controllers/SubmissionsController.cs
@@ -1,11 +1,44 @@
-﻿using SIS.MvcFramework;
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+using SulsApp.ViewModels.Submissions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SulsApp.Controllers
 {
     public class SubmissionsController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public SubmissionsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public HttpResponse Create(string id)
+        {
+            if (!this.IsUserLoggedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var problemView = this.db.Problems
+                .Where(p => p.Id == id)
+                .Select(p => new CreateFormViewModel
+                {
+                    ProblemId = p.Id,
+                    Name = p.Name
+                }).FirstOrDefault();
+
+            return this.View(problemView);
+        }
+
+        [HttpPost]
+        public HttpResponse Create()
+        {
+            return this.Redirect("/");
+        }
     }
 }
---
+++ C#WebDevelopment/C#-Web-Basics/ExamPreparationSULSJan2020/src/SulsApp/ViewModels/Submissions/CreateFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SulsApp.ViewModels.Submissions
+{
+    public class CreateFormViewModel
+    {
+        public string ProblemId { get; set; }
+
+        public string Name { get; set; }
+    }
+}

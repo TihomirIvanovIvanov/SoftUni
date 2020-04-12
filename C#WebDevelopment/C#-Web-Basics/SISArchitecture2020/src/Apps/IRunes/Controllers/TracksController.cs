2e9655657ffe909b28679fdce244a3ce89605753
tihomir <tihomir_2005@abv.bg>
2020-04-12 12:32:09 +0300

    Add post for create a track.

--- C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/IRunes/Controllers/TracksController.cs
+++ C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/IRunes/Controllers/TracksController.cs
@@ -1,13 +1,60 @@
-﻿using SIS.HTTP;
+﻿using IRunes.Services;
+using IRunes.ViewModels.Tracks;
+using SIS.HTTP;
 using SIS.MvcFramework;
 
 namespace IRunes.Controllers
 {
     public class TracksController : Controller
     {
-        public HttpResponse Create()
+        private readonly ITracksService tracksService;
+
+        public TracksController(ITracksService tracksService)
         {
-            return this.View();
+            this.tracksService = tracksService;
+        }
+
+        public HttpResponse Create(string albumId)
+        {
+            if (!this.IsUserLoggedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var viewModel = new CreateViewModel
+            {
+                AlbumId = albumId
+            };
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public HttpResponse Create(CreateInputModel input)
+        {
+            if (!this.IsUserLoggedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            if (input.Name.Length < 4 || input.Name.Length > 20)
+            {
+                return this.Create(input.AlbumId);
+            }
+
+            if (string.IsNullOrWhiteSpace(input.Link))
+            {
+                return this.Create(input.AlbumId);
+            }
+
+            if (input.Price < 0)
+            {
+                return this.Create(input.AlbumId);
+            }
+
+            this.tracksService.Create(input.AlbumId, input.Name, input.Link, input.Price);
+
+            return this.Redirect("/Albums/Details?id=" + input.AlbumId);
         }
 
         public HttpResponse Details()
--- C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/IRunes/Services/TracksService.cs
+++ C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/IRunes/Services/TracksService.cs
@@ -1,6 +1,38 @@
-﻿namespace IRunes.Services
+﻿using IRunes.Data;
+using IRunes.Models;
+using System.Linq;
+
+namespace IRunes.Services
 {
     public class TracksService : ITracksService
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public TracksService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(string albumId, string name, string link, decimal price)
+        {
+            var track = new Track
+            {
+                AlbumId = albumId,
+                Name = name,
+                Link = link,
+                Price = price
+            };
+
+            this.dbContext.Tracks.Add(track);
+
+            var allTracksPricesSum = this.dbContext.Tracks
+                .Where(track => track.AlbumId == albumId)
+                .Sum(track => track.Price);
+
+            var album = this.dbContext.Albums.FirstOrDefault(album => album.Id == albumId);
+            album.Price = allTracksPricesSum * 0.87m;
+
+            this.dbContext.SaveChanges();
+        }
     }
 }

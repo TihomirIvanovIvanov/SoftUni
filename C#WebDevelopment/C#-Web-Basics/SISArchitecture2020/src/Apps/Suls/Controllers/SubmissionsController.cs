71f6fc95db1beed4baf02172531cde14e8c80ee1
tihomir <tihomir_2005@abv.bg>
2020-04-19 12:14:13 +0300

    Implement get for create a submission

---
+++ C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/Suls/Controllers/SubmissionsController.cs
@@ -0,0 +1,34 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+using Suls.Services;
+using Suls.ViewModels.Submissions;
+
+namespace Suls.Controllers
+{
+    public class SubmissionsController : Controller
+    {
+        private readonly IProblemsService problemsService;
+
+        public SubmissionsController(IProblemsService problemsService)
+        {
+            this.problemsService = problemsService;
+        }
+
+        public HttpResponse Create(string id)
+        {
+            if (!this.IsUserLoggedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var problem = this.problemsService.GetById(id);
+            var viewModel = new CreateViewModel
+            {
+                ProblemId = problem.Id,
+                Name = problem.Name,
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}

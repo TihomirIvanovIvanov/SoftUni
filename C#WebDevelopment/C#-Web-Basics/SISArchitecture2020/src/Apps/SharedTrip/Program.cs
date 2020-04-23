4d6a025218b862b539363fbf189c2b46e0259894
tihomir <tihomir_2005@abv.bg>
2020-04-23 14:10:45 +0300

    Addd initial architecture of shared trip exam

---
+++ C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/SharedTrip/Program.cs
@@ -0,0 +1,16 @@
+﻿using SIS.MvcFramework;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharedTrip
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            await WebHost.StartAsync(new Startup());
+        }
+    }
+}
---
+++ C#WebDevelopment/C#-Web-Basics/SISArchitecture2020/src/Apps/SharedTrip/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedTrip
+{
+    using SIS.HTTP;
+    using SIS.MvcFramework;
+    using System.Collections.Generic;
+
+    public class Startup : IMvcApplication
+    {
+        public void Configure(IList<Route> routeTable)
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+
+        }
+    }
+}

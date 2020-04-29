913e3522c415f7cf9d0cba0302b877a301ad5fe5
tihomir <tihomir_2005@abv.bg>
2020-04-29 11:34:49 +0300

    Implement user functionality

---
+++ C#WebDevelopment/C#-Web-Basics/ExamRetakeBattleCards2020/src/BattleCards/Services/IUsersService.cs
@@ -0,0 +1,17 @@
+﻿using BattleCards.ViewModels.Users;
+
+namespace BattleCards.Services
+{
+    public interface IUsersService
+    {
+        string GetUserId(LoginInputModel input);
+
+        void Register(RegisterInputModel input);
+
+        bool UsernameExists(string username);
+
+        bool EmailExists(string email);
+
+        string GetUsername(string id);
+    }
+}
---
+++ C#WebDevelopment/C#-Web-Basics/ExamRetakeBattleCards2020/src/BattleCards/Services/UsersService.cs
@@ -0,0 +1,88 @@
+﻿using BattleCards.Data;
+using BattleCards.Models;
+using BattleCards.ViewModels.Users;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BattleCards.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UsersService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public bool EmailExists(string email)
+        {
+            var emailExist = this.dbContext.Users.Any(user => user.Email == email);
+            return emailExist;
+        }
+
+        public string GetUserId(LoginInputModel input)
+        {
+            var hashPassword = this.Hash(input.Password);
+            var user = this.dbContext.Users
+                .FirstOrDefault(user => user.Username == input.Username && user.Password == hashPassword);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user.Id;
+        }
+
+        public string GetUsername(string id)
+        {
+            var username = this.dbContext.Users
+                .Where(user => user.Id == id)
+                .Select(user => user.Username)
+                .FirstOrDefault();
+
+            return username;
+        }
+
+        public void Register(RegisterInputModel input)
+        {
+            var hashPassword = this.Hash(input.Password);
+
+            var user = new User
+            {
+                Username = input.Username,
+                Email = input.Email,
+                Password = hashPassword,
+            };
+
+            this.dbContext.Users.Add(user);
+            this.dbContext.SaveChanges();
+        }
+
+        public bool UsernameExists(string username)
+        {
+            var usernameExist = this.dbContext.Users.Any(user => user.Username == username);
+            return usernameExist;
+        }
+
+        private string Hash(string input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(input));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+    }
+}

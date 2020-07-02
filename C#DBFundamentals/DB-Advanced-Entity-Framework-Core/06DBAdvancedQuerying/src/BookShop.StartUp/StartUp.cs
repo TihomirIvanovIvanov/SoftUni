9bdc0882ec46c9e05d458982909ccc5e7110701e
tihomir <tihomir_2005@abv.bg>
2020-07-02 16:01:14 +0300

    Recap add 14. task Increase Prices from advanced querying exercises

--- C#DBFundamentals/DB-Advanced-Entity-Framework-Core/06DBAdvancedQuerying/src/BookShop.StartUp/StartUp.cs
+++ C#DBFundamentals/DB-Advanced-Entity-Framework-Core/06DBAdvancedQuerying/src/BookShop.StartUp/StartUp.cs
@@ -17,7 +17,7 @@
 
             using (var db = new BookShopContext())
             {
-                Console.WriteLine(GetMostRecentBooks(db));
+                IncreasePrices(db);
             }
         }
 
@@ -232,6 +232,16 @@
             }
 
             return builder.ToString().TrimEnd();
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            context.Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010)
+                .ToList()
+                .ForEach(b => b.Price += 5);
+
+            context.SaveChanges();
         }
     }
 }

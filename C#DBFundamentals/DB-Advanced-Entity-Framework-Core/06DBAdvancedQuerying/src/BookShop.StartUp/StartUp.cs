af1d5dfb2fec10e5ea5324d05d2c5fa9171a7302
tihomir <tihomir_2005@abv.bg>
2020-07-02 13:24:20 +0300

    Recap add 1 task Age Restriction from advanced querying exercises

--- C#DBFundamentals/DB-Advanced-Entity-Framework-Core/06DBAdvancedQuerying/src/BookShop.StartUp/StartUp.cs
+++ C#DBFundamentals/DB-Advanced-Entity-Framework-Core/06DBAdvancedQuerying/src/BookShop.StartUp/StartUp.cs
@@ -2,15 +2,48 @@
 {
     using BookShop.Data;
     using BookShop.Initializer;
-    
-    class StartUp
+    using BookShop.Models;
+    using System;
+    using System.Linq;
+
+    public class StartUp
     {
-        static void Main()
+        public static void Main()
         {
+            var input = Console.ReadLine();
+
             using (var db = new BookShopContext())
             {
-                DbInitializer.ResetDatabase(db);
+                Console.WriteLine(GetBooksByAgeRestriction(db, input));
             }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            int enumValue = -1;
+
+            switch (command.ToLower())
+            {
+                case "minor":
+                    enumValue = 0;
+                    break;
+                case "teen":
+                    enumValue = 1;
+                    break;
+                case "adult":
+                    enumValue = 2;
+                    break;
+            }
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == (AgeRestriction)enumValue)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            var result = String.Join(Environment.NewLine, books);
+
+            return result;
         }
     }
 }

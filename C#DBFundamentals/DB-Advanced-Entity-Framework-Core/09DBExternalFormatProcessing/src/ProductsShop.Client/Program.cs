364b0b9557924379edd705cea02b01b0d39bbff3
tihomir <tihomir_2005@abv.bg>
2020-07-18 18:58:44 +0300

    Recap import users /External Format Processing exercises/

--- C#DBFundamentals/DB-Advanced-Entity-Framework-Core/09DBExternalFormatProcessing/src/ProductsShop.Client/Program.cs
+++ C#DBFundamentals/DB-Advanced-Entity-Framework-Core/09DBExternalFormatProcessing/src/ProductsShop.Client/Program.cs
@@ -15,8 +15,33 @@ namespace ProductsShop.Client
         {
             using (var context = new ProductsShopContext())
             {
-                context.Database.EnsureCreated();
+                SeedJson(context);
             }
+        }
+
+        private static void SeedJson(ProductsShopContext context)
+        {
+            var users = ImportUsersJson();
+            context.Users.AddRange(users);
+
+            //var categories = ImportCategoriesJson();
+            //context.Categories.AddRange(categories);
+
+            //var products = ImportProductsJson(context);
+            //context.Products.AddRange(products);
+
+            //var categoryProducts = CreateCategoryProducts(context);
+            //context.CategoryProducts.AddRange(categoryProducts);
+
+            context.SaveChanges();
+        }
+
+        private static User[] ImportUsersJson()
+        {
+            var json = File.ReadAllText(@"C:\Users\tihom\source\SoftUniCoursesCSharp\00GitAndGitHub\SoftUni\C#DBFundamentals\DB-Advanced-Entity-Framework-Core\09DBExternalFormatProcessing\src\ProductsShop.Client\Import\users.json");
+            var users = JsonConvert.DeserializeObject<User[]>(json);
+
+            return users;
         }
     }
 }

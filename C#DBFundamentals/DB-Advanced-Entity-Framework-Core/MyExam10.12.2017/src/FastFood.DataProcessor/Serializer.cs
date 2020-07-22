5b578a42a772f49572c5a314d31b0e2d8b53d128
tihomir <tihomir_2005@abv.bg>
2020-07-23 02:16:04 +0300

    Recap add Export Categories with their Most Popular Item from Xml /my exam 10.12.2017 FastFood app/

--- C#DBFundamentals/DB-Advanced-Entity-Framework-Core/MyExam10.12.2017/src/FastFood.DataProcessor/Serializer.cs
+++ C#DBFundamentals/DB-Advanced-Entity-Framework-Core/MyExam10.12.2017/src/FastFood.DataProcessor/Serializer.cs
@@ -1,8 +1,13 @@
 ﻿using FastFood.Data;
+using FastFood.DataProcessor.Dto.Export;
 using FastFood.Models;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace FastFood.DataProcessor
 {
@@ -39,12 +44,58 @@ namespace FastFood.DataProcessor
                 TotalMade = ordersByEmployee.Sum(o => o.TotalPrice)
             };
 
-            return JsonConvert.SerializeObject(result, Formatting.Indented);
+            return JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
         }
 
         public static string ExportCategoryStatistics(FastFoodDbContext context, string categoriesString)
         {
-            throw new Exception();
+            var categoriesAsArray = categoriesString.Split(',').ToArray();
+
+            var cagetories = context.Categories
+                .Where(c => categoriesAsArray.Contains(c.Name))
+                .Select(c => new
+                {
+                    c.Name,
+                    Items = c.Items.ToList()
+                })
+                .ToList()
+                .Select(c => new
+                {
+                    c.Name,
+                    MostPopularItem = c.Items
+                    .Select(i => new
+                    {
+                        i.Name,
+                        TotalMade = i.Price * i.OrderItems.Sum(oi => oi.Quantity),
+                        TimesSold = i.OrderItems.Sum(oi => oi.Quantity)
+                    })
+                    .ToList()
+                    .OrderByDescending(i => i.TotalMade)
+                    .ThenByDescending(i => i.TimesSold)
+                    .FirstOrDefault()
+                })
+                .Select(c => new CategoryDtoXml
+                {
+                    Name = c.Name,
+                    MostPopularItem = new MostPopularItemDtoXml
+                    {
+                        Name = c.MostPopularItem.Name,
+                        TotalMade = c.MostPopularItem.TotalMade,
+                        TimesSold = c.MostPopularItem.TimesSold
+                    }
+                })
+                .OrderByDescending(c => c.MostPopularItem.TotalMade)
+                .ThenByDescending(c => c.MostPopularItem.TimesSold)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            var serializer = new XmlSerializer(typeof(CategoryDtoXml[]), new XmlRootAttribute("Categories"));
+            serializer.Serialize
+                (new StringWriter(sb), cagetories, new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty }));
+
+            var result = sb.ToString();
+            return result;
         }
     }
 }

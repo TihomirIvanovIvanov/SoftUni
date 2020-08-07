22a7c69f7c64325124f910327667f67fa53bc970
tihomir <tihomir_2005@abv.bg>
2020-08-08 00:23:19 +0300

    Recap add 1 task from exercises

---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MultidimensionalArraysExercises/SumMatrixElements/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            var matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            var sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}

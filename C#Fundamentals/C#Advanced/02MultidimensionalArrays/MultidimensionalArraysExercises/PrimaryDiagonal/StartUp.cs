7b519069efbbf2b6e50cbbad5632e107c0b1d73b
tihomir <tihomir_2005@abv.bg>
2020-08-08 01:15:54 +0300

    Recap add 2, 3 task from exercises

---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MultidimensionalArraysExercises/PrimaryDiagonal/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            var primarySum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                primarySum += matrix[row, row];
+            }
+
+            Console.WriteLine(primarySum);
+        }
+    }
+}

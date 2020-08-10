e8b9ef9428d3b4c6de477aa8c01af8467c993a0f
tihomir <tihomir_2005@abv.bg>
2020-08-10 17:57:24 +0300

    Recap add 4, 5 task from more exercises

---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MoreExercises/MaximalSum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var matrix = new int[sizes[0], sizes[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var colElems = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = colElems[j];
+                }
+            }
+
+            var maxSum = int.MinValue;
+            var rowIndex = 0;
+            var colIndex = 0;
+
+            for (int i = 0; i < matrix.GetLength(0) - 2; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 2; j++)
+                {
+                    var sum = matrix[i, j] + matrix[i, j + 1] + matrix[i, j + 2] +
+                              matrix[i + 1, j] + matrix[i + 1, j + 1] + matrix[i + 1, j + 2] +
+                              matrix[i + 2, j] + matrix[i + 2, j + 1] + matrix[i + 2, j + 2];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        rowIndex = i;
+                        colIndex = j;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int i = rowIndex; i < rowIndex + 3; i++)
+            {
+                for (int j = colIndex; j < colIndex + 3; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MoreExercises/RubiksMatrix/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+
+namespace RubiksMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = sizes[0];
+            var cols = sizes[1];
+
+            var rubikMatrix = new int[rows][];
+
+            GetMatrix(rubikMatrix, cols);
+
+            var commandsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commandsCount; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var rowColIndex = int.Parse(input[0]);
+                var direction = input[1];
+                var moves = int.Parse(input[2]);
+
+                if (direction == "down")
+                {
+                    MoveDown(rubikMatrix, rowColIndex, moves % rubikMatrix.Length);
+                }
+                else if (direction == "left")
+                {
+                    MoveLeft(rubikMatrix, rowColIndex, moves % rubikMatrix[0].Length);
+                }
+                else if (direction == "right")
+                {
+                    MoveRight(rubikMatrix, rowColIndex, moves % rubikMatrix[0].Length);
+                }
+                else if (direction == "up")
+                {
+                    MoveUp(rubikMatrix, rowColIndex, moves % rubikMatrix.Length);
+                }
+            }
+
+            var counter = 1;
+
+            for (int row = 0; row < rubikMatrix.Length; row++)
+            {
+                for (int col = 0; col < rubikMatrix.Length; col++)
+                {
+                    if (rubikMatrix[row][col] == counter)
+                    {
+                        Console.WriteLine("No swap required");
+                        counter++;
+                    }
+                    else
+                    {
+                        Rearrange(rubikMatrix, row, col, counter);
+                        counter++;
+                    }
+                }
+            }
+        }
+
+        private static void Rearrange(int[][] rubikMatrix, int row, int col, int counter)
+        {
+            for (int targetRow = 0; targetRow < rubikMatrix.Length; targetRow++)
+            {
+                for (int targetCol = 0; targetCol < rubikMatrix[targetRow].Length; targetCol++)
+                {
+                    if (rubikMatrix[targetRow][targetCol] == counter)
+                    {
+                        rubikMatrix[targetRow][targetCol] = rubikMatrix[row][col];
+                        rubikMatrix[row][col] = counter;
+                        Console.WriteLine($"Swap ({row}, {col}) with ({targetRow}, {targetCol})");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void MoveUp(int[][] rubikMatrix, int col, int moves)
+        {
+            for (int i = 0; i < moves; i++)
+            {
+                var firstElement = rubikMatrix[0][col];
+
+                for (int row = 0; row < rubikMatrix.Length - 1; row++)
+                {
+                    rubikMatrix[row][col] = rubikMatrix[row + 1][col];
+                }
+
+                rubikMatrix[rubikMatrix.Length - 1][col] = firstElement;
+            }
+        }
+
+        private static void MoveRight(int[][] rubikMatrix, int row, int moves)
+        {
+            for (int i = 0; i < moves; i++)
+            {
+                var lastElement = rubikMatrix[row][rubikMatrix[row].Length - 1];
+
+                for (int col = rubikMatrix.Length - 1; col > 0; col--)
+                {
+                    rubikMatrix[row][col] = rubikMatrix[row][col - 1];
+                }
+
+                rubikMatrix[row][0] = lastElement;
+            }
+        }
+
+        private static void MoveLeft(int[][] rubikMatrix, int row, int moves)
+        {
+            for (int i = 0; i < moves; i++)
+            {
+                var firstElement = rubikMatrix[row][0];
+
+                for (int col = 0; col < rubikMatrix[row].Length - 1; col++)
+                {
+                    rubikMatrix[row][col] = rubikMatrix[row][col + 1];
+                }
+
+                rubikMatrix[row][rubikMatrix[row].Length - 1] = firstElement;
+            }
+        }
+
+        private static void MoveDown(int[][] rubikMatrix, int col, int moves)
+        {
+            for (int i = 0; i < moves; i++)
+            {
+                var lastElement = rubikMatrix[rubikMatrix.Length - 1][col];
+
+                for (int row = rubikMatrix.Length - 1; row > 0; row--)
+                {
+                    rubikMatrix[row][col] = rubikMatrix[row - 1][col];
+                }
+
+                rubikMatrix[0][col] = lastElement;
+            }
+        }
+
+        private static void GetMatrix(int[][] rubikMatrix, int cols)
+        {
+            var counter = 1;
+
+            for (int row = 0; row < rubikMatrix.Length; row++)
+            {
+                rubikMatrix[row] = new int[cols];
+
+                for (int col = 0; col < rubikMatrix[row].Length; col++)
+                {
+                    rubikMatrix[row][col] = counter++;
+                }
+            }
+        }
+    }
+}

cfd57e3dddb0772768be37f804a58d4e4ca79941
tihomir <tihomir_2005@abv.bg>
2020-08-10 16:43:17 +0300

    Recap add 2, 3 task from more exercises

---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MoreExercises/DiagonalDifference/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var matrix = new int[n][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            var primarySum = int.MinValue;
+            var secondarySym = int.MinValue;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                primarySum += matrix[i][i];
+                secondarySym += matrix[i][matrix.Length - 1 - i];
+            }
+
+            var result = Math.Abs(primarySum - secondarySym);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MoreExercises/SquaresInMatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var matrix = new char[sizes[0], sizes[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var colElems = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = colElems[j];
+                }
+            }
+
+            var counter = 0;
+
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    var areEquals = matrix[i, j] == matrix[i, j + 1] &&
+                                    matrix[i, j] == matrix[i + 1, j + 1] &&
+                                    matrix[i, j] == matrix[i + 1, j];
+
+                    if (areEquals)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}

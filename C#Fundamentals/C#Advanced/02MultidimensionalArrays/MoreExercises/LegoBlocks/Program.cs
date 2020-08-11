0f5c72d7d64a84002f3d501593c8e54dc6360124
tihomir <tihomir_2005@abv.bg>
2020-08-11 16:43:42 +0300

    Recap add LegoBlocks and RadioactiveBunnies tasks from more exercises

---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MoreExercises/LegoBlocks/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace LegoBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine());
+
+            var totalCells = 0;
+
+            var firstMatrix = new int[rows][];
+            var secondMatrix = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                firstMatrix[i] = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                totalCells += firstMatrix[i].Length;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                secondMatrix[i] = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Reverse()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                totalCells += secondMatrix[i].Length;
+            }
+
+            var colLength = firstMatrix[0].Length + secondMatrix[0].Length;
+            var isFit = true;
+
+            for (int i = 0; i < firstMatrix.Length; i++)
+            {
+                if (firstMatrix[i].Length + secondMatrix[i].Length != colLength)
+                {
+                    isFit = false;
+                    break;
+                }
+            }
+
+            if (isFit)
+            {
+                for (int i = 0; i < secondMatrix.Length; i++)
+                {
+                    Console.WriteLine($"[{string.Join(", ", firstMatrix[i])}, {string.Join(", ", secondMatrix[i])}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {totalCells}");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/02MultidimensionalArrays/MoreExercises/RadioactiveBunnies/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RadioactiveBunnies
+{
+    class Program
+    {
+        static int playerRow;
+
+        static int playerCol;
+
+        static char[][] jaggedArray;
+
+        static bool isOutside;
+
+        static bool isDead;
+
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = sizes[0];
+            var cols = sizes[1];
+
+            jaggedArray = new char[rows][];
+
+            GetJaggedArray(cols);
+
+            var directions = Console.ReadLine();
+
+            MovePlayer(directions);
+        }
+
+        private static void MovePlayer(string directions)
+        {
+            for (int i = 0; i < directions.Length; i++)
+            {
+                var currentStep = directions[i];
+
+                if (currentStep == 'U')
+                {
+                    Move(-1, 0);
+                }
+                else if (currentStep == 'L')
+                {
+                    Move(0, -1);
+                }
+                else if (currentStep == 'R')
+                {
+                    Move(0, 1);
+                }
+                else if (currentStep == 'D')
+                {
+                    Move(1, 0);
+                }
+
+                Spread();
+
+                if (isDead)
+                {
+                    PrintJaggedArray();
+                    Console.WriteLine($"dead: {playerRow} {playerCol}");
+                    break;
+                }
+                else if (isOutside)
+                {
+                    PrintJaggedArray();
+                    Console.WriteLine($"won: {playerRow} {playerCol}");
+                    break;
+                }
+            }
+        }
+
+        private static void PrintJaggedArray()
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine(string.Join(string.Empty, jaggedArray[i]));
+            }
+        }
+
+        private static void Spread()
+        {
+            var indexes = new Queue<int[]>();
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    if (jaggedArray[i][j] == 'B')
+                    {
+                        indexes.Enqueue(new int[] { i, j });
+                    }
+                }
+            }
+
+            while (indexes.Count > 0)
+            {
+                var currentIndex = indexes.Dequeue();
+
+                var targetRow = currentIndex[0];
+                var targetCol = currentIndex[1];
+
+                if (IsInside(targetRow - 1, targetCol))
+                {
+                    if (IsPlayer(targetRow - 1, targetCol))
+                    {
+                        isDead = true;
+                    }
+
+                    jaggedArray[targetRow - 1][targetCol] = 'B';
+                }
+
+                if (IsInside(targetRow + 1, targetCol))
+                {
+                    if (IsPlayer(targetRow + 1, targetCol))
+                    {
+                        isDead = true;
+                    }
+
+                    jaggedArray[targetRow + 1][targetCol] = 'B';
+                }
+
+                if (IsInside(targetRow, targetCol - 1))
+                {
+                    if (IsPlayer(targetRow, targetCol - 1))
+                    {
+                        isDead = true;
+                    }
+
+                    jaggedArray[targetRow][targetCol - 1] = 'B';
+                }
+
+                if (IsInside(targetRow, targetCol + 1))
+                {
+                    if (IsPlayer(targetRow, targetCol + 1))
+                    {
+                        isDead = true;
+                    }
+
+                    jaggedArray[targetRow][targetCol + 1] = 'B';
+                }
+            }
+        }
+
+        private static bool IsPlayer(int row, int col)
+        {
+            return jaggedArray[row][col] == 'P';
+        }
+
+        private static void Move(int row, int col)
+        {
+            var targetRow = playerRow + row;
+            var targetCol = playerRow + col;
+
+            if (!IsInside(targetRow, targetCol))
+            {
+                jaggedArray[playerRow][playerCol] = '.';
+                isOutside = true;
+            }
+            else if (IsBunny(targetRow, targetCol))
+            {
+                jaggedArray[playerRow][playerCol] = '.';
+                playerRow += row;
+                playerCol += col;
+                isDead = true;
+            }
+            else
+            {
+                jaggedArray[playerRow][playerCol] = '.';
+                playerRow += row;
+                playerCol += col;
+
+                jaggedArray[playerRow][playerCol] = 'P';
+            }
+        }
+
+        private static bool IsBunny(int targetRow, int targetCol)
+        {
+            return jaggedArray[targetRow][targetCol] == 'B';
+        }
+
+        private static bool IsInside(int row, int col)
+        {
+            return row >= 0 && row < jaggedArray.Length &&
+                   col >= 0 && col < jaggedArray[row].Length;
+        }
+
+        private static void GetJaggedArray(int cols)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                var input = Console.ReadLine();
+
+                jaggedArray[i] = new char[cols];
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = input[j];
+
+                    if (jaggedArray[i][j] == 'P')
+                    {
+                        playerRow = i;
+                        playerCol = j;
+                    }
+                }
+            }
+        }
+    }
+}

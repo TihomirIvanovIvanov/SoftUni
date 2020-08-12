aecae64f3d772afbdca9dc4a5099ba7adca80a91
tihomir <tihomir_2005@abv.bg>
2020-08-12 22:28:32 +0300

    Recap add 2 and 3 task from sets and dictionaries lab

---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictLab/AverageStudentGrades/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (students.ContainsKey(input[0]))
+                {
+                    students[input[0]].Add(double.Parse(input[1]));
+                }
+                else
+                {
+                    students[input[0]] = new List<double>
+                    {
+                        double.Parse(input[1])
+                    };
+                }
+            }
+
+            foreach (var student in students)
+            {
+                var grades = student.Value.Select(x => x.ToString("F2"));
+
+                Console.WriteLine($"{student.Key} -> {string.Join(' ', grades)} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictLab/ProductShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "Revision")
+            {
+                if (!shops.ContainsKey(input[0]))
+                {
+                    shops.Add(input[0], new Dictionary<string, double>());
+                }
+
+                if (!shops[input[0]].ContainsKey(input[1]))
+                {
+                    shops[input[0]].Add(input[1], 0);
+                }
+
+                shops[input[0]][input[1]] = double.Parse(input[2]);
+
+                input = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {item.Value}");
+                }
+            }
+        }
+    }
+}

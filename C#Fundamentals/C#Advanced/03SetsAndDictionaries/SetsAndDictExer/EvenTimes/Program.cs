be9856b901a82489c622357ed68b018497c4f8b8
tihomir <tihomir_2005@abv.bg>
2020-08-12 21:20:09 +0300

    Recap add 3 and 4 task from sets and dictionaries exercises

---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/EvenTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currentNumber))
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+
+                numbers[currentNumber] += 1;
+            }
+
+            Console.WriteLine(numbers
+                .Where(n => n.Value % 2 == 0)
+                .FirstOrDefault()
+                .Key);
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentElem = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var elem in currentElem)
+                {
+                    elements.Add(elem);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', elements));
+        }
+    }
+}

0a02e997c7604538d80a92b2e0052ecd113bc638
tihomir <tihomir_2005@abv.bg>
2020-08-12 21:06:38 +0300

    Recap add 1 and 2 task from sets and dictionaries exercises

---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/SetsOfElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = length[0];
+            var m = length[1];
+
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                firstSet.Add(currentNumber);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                secondSet.Add(currentNumber);
+            }
+
+            var intersectElements = firstSet.Intersect(secondSet);
+
+            Console.WriteLine(string.Join(' ', intersectElements));
+        }
+    }
+}

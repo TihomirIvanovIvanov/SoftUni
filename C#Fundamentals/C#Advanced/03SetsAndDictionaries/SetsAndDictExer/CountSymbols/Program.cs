45f95dcd65244a0c44a5d2ab78fc36a122f8cc1d
tihomir <tihomir_2005@abv.bg>
2020-08-12 21:33:41 +0300

    Recap add 5 and 6 task from sets and dictionaries exercises

---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var symbols = new SortedDictionary<char, int>();
+
+            foreach (var currentCharacter in input)
+            {
+                if (!symbols.ContainsKey(currentCharacter))
+                {
+                    symbols.Add(currentCharacter, 0);
+                }
+
+                symbols[currentCharacter] += 1;
+            }
+
+            foreach (var item in symbols)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/Wardrobe/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                var color = input[0];
+                var clothes = input[1]
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                for (int j = 0; j < clothes.Length; j++)
+                {
+                    var currentItem = clothes[j];
+
+                    if (!wardrobe[color].ContainsKey(currentItem))
+                    {
+                        wardrobe[color].Add(currentItem, 0);
+                    }
+
+                    wardrobe[color][currentItem] += 1;
+                }
+            }
+
+            var targetItem = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var targetColor = targetItem[0];
+            var itemName = targetItem[1];
+
+            foreach (var kvp in wardrobe)
+            {
+                Console.WriteLine($"{kvp.Key} clothes:");
+
+                foreach (var item in kvp.Value)
+                {
+                    Console.Write($"* {item.Key} - {item.Value}");
+
+                    if (kvp.Key == targetColor && item.Key == itemName)
+                    {
+                        Console.Write(" (found!)");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}

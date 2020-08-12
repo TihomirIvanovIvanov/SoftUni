b00887e0e6f01869446f8e8a50f2d0ecbe53ee8e
tihomir <tihomir_2005@abv.bg>
2020-08-12 22:05:12 +0300

    Recap add 7 and 8 task from sets and dictionaries exercises

---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/Ranking/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contests = new Dictionary<string, string>();
+            var students = new Dictionary<string, Dictionary<string, int>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                var elements = input
+                    .Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+                var contestName = elements[0];
+                var contestPassword = elements[1];
+
+                if (!contests.ContainsKey(contestName))
+                {
+                    contests.Add(contestName, contestPassword);
+                }
+            }
+
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                var elements = input
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+                var contestName = elements[0];
+                var contestPassword = elements[1];
+                var username = elements[2];
+                var currentPoints = int.Parse(elements[3]);
+
+                if (contests.ContainsKey(contestName) && contests[contestName] == contestPassword)
+                {
+                    if (!students.ContainsKey(username))
+                    {
+                        students.Add(username, new Dictionary<string, int>());
+                    }
+
+                    if (!students[username].ContainsKey(contestName))
+                    {
+                        students[username].Add(contestName, currentPoints);
+                    }
+
+                    if (students[username][contestName] < currentPoints)
+                    {
+                        students[username][contestName] = currentPoints;
+                    }
+                }
+            }
+
+            var topStudent = students
+                .OrderByDescending(s => s.Value.Sum(sum => sum.Value))
+                .FirstOrDefault();
+
+            var bestPoints = topStudent
+                .Value
+                .Sum(s => s.Value);
+
+            Console.WriteLine($"Best candidate is {topStudent.Key} with total {bestPoints} points.");
+
+            Console.WriteLine("Ranking:");
+
+            var sortedStudents = students.OrderBy(s => s.Key)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var kvp in sortedStudents)
+            {
+                Console.WriteLine(kvp.Key);
+
+                foreach (var contest in kvp.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictExer/TheVLogger/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheVLogger
+{
+    class Program
+    {
+        static string following = "following";
+
+        static string followers = "followers";
+
+        static void Main(string[] args)
+        {
+            var vloggers = new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                var elems = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var user = elems[0];
+                var command = elems[1];
+                var targetUser = elems[2];
+
+                if (command == "joined")
+                {
+                    if (!vloggers.ContainsKey(user))
+                    {
+                        vloggers.Add(user, new Dictionary<string, SortedSet<string>>());
+                        vloggers[user].Add(following, new SortedSet<string>());
+                        vloggers[user].Add(followers, new SortedSet<string>());
+                    }
+                }
+                else if (command == "followed")
+                {
+                    var isSamePerson = user == targetUser;
+
+                    if (vloggers.ContainsKey(user) && vloggers.ContainsKey(targetUser) && !isSamePerson)
+                    {
+                        vloggers[user][following].Add(targetUser);
+                        vloggers[targetUser][followers].Add(user);
+                    }
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+
+            var sortedVloggers = vloggers
+                .OrderByDescending(v => v.Value[followers].Count)
+                .ThenBy(v => v.Value[following].Count);
+
+            var counter = 1;
+
+            foreach (var vlogger in sortedVloggers)
+            {
+                Console.WriteLine($"{counter}. {vlogger.Key} : {vlogger.Value[followers].Count} followers, {vlogger.Value[following].Count} following");
+
+                if (counter == 1)
+                {
+                    foreach (var followerName in vlogger.Value[followers])
+                    {
+                        Console.WriteLine($"*  {followerName}");
+                    }
+                }
+
+                counter++;
+            }
+        }
+    }
+}

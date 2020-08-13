06dc08c4dd79a3593133d11d6b3da2cb93a9e622
tihomir <tihomir_2005@abv.bg>
2020-08-13 17:14:12 +0300

    Recap add 7 and 8 task from Stacks And Queues exercises

---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesExercise/StackFibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var fibonacci = new Stack<long>();
+
+            fibonacci.Push(0);
+            fibonacci.Push(1);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                var firstNumber = fibonacci.Pop();
+                var secondNumber = fibonacci.Peek();
+
+                fibonacci.Push(firstNumber);
+                fibonacci.Push(firstNumber + secondNumber);
+            }
+
+            Console.WriteLine(fibonacci.Peek());
+        }
+    }
+}

599d6583abf981020aef581d1101e16e36f11807
tihomir <tihomir_2005@abv.bg>
2020-08-13 13:19:08 +0300

    Recap add 4 and 5 task from sets and dictionaries lab

---
+++ C#Fundamentals/C#Advanced/03SetsAndDictionaries/SetsAndDictLab/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var continent = input[0];
+                var country = input[1];
+                var city = input[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                continents[continent][country].Add(city);
+            }
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var item in continent.Value)
+                {
+                    Console.WriteLine($"  {item.Key} -> {string.Join(", ", item.Value)}");
+                }
+            }
+        }
+    }
+}

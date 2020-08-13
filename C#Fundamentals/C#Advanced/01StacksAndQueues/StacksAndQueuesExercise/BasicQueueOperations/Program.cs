df9c750bb22c1d9bbc48228fc7d3bc78b6968f98
tihomir <tihomir_2005@abv.bg>
2020-08-13 16:56:18 +0300

    Recap add 4, 5 and 6 task from Stacks And Queues exercises

---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesExercise/BasicQueueOperations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operations = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var toPush = operations[0];
+            var toPop = operations[1];
+            var magicNumber = operations[2];
+
+            var queueOfNumbers = new Queue<int>();
+
+            for (int i = 0; i < toPush && i < numbers.Length; i++)
+            {
+                queueOfNumbers.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < toPop && queueOfNumbers.Count > 0; i++)
+            {
+                queueOfNumbers.Dequeue();
+            }
+
+            if (queueOfNumbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queueOfNumbers.Contains(magicNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queueOfNumbers.Min());
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesExercise/TruckTour/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var petrolPumps = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                petrolPumps.Enqueue(input);
+            }
+
+            var index = 0;
+
+            while (true)
+            {
+                var totalFuel = 0;
+
+                foreach (var currentPetrolPump in petrolPumps)
+                {
+                    var fuel = currentPetrolPump[0];
+                    var distance = currentPetrolPump[1];
+
+                    totalFuel += fuel - distance;
+
+                    if (totalFuel < 0)
+                    {
+                        index++;
+
+                        var pumpForRemove = petrolPumps.Dequeue();
+                        petrolPumps.Enqueue(pumpForRemove);
+                        break;
+                    }
+                }
+
+                if (totalFuel >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}

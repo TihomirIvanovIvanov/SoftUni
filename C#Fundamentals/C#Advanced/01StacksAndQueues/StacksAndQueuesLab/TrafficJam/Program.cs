c04b632422f124b050aa9d9cc0265cc92370bdf8
tihomir <tihomir_2005@abv.bg>
2020-08-13 16:11:31 +0300

    Recap add 4,5 and 6 task from StacksAndQueues lab

---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesLab/TrafficJam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            var count = 0;
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count > 0)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            count++;
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}

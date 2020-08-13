91f63ca23d1c4cb16c2ce64d9e8e9a17e63dfcbb
tihomir <tihomir_2005@abv.bg>
2020-08-13 17:30:39 +0300

    Recap add 9 and 10 task from Stacks And Queues exercises

---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesExercise/PoisonousPlants/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PoisonousPlants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var plants = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var indexes = new List<int>();
+
+            var days = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < plants.Count - 1; i++)
+                {
+                    if (plants[i] < plants[i + 1])
+                    {
+                        indexes.Add(i + 1);
+                    }
+                }
+
+                if (indexes.Count == 0)
+                {
+                    break;
+                }
+
+                var counter = 0;
+
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    plants.RemoveAt(indexes[i] - counter);
+                    counter++;
+                }
+
+                days++;
+                indexes.Clear();
+            }
+
+            Console.WriteLine(days);
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesExercise/SimpleTextEditor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operationsCount = int.Parse(Console.ReadLine());
+
+            var text = string.Empty;
+            var stack = new Stack<string>();
+
+            for (int i = 0; i < operationsCount; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var currentCommand = input[0];
+
+                if (currentCommand == "1")
+                {
+                    var currentText = input[1];
+                    stack.Push(text);
+
+                    text += currentText;
+                }
+                else if (currentCommand == "2")
+                {
+                    var count = int.Parse(input[1]);
+
+                    if (count > text.Length)
+                    {
+                        count = Math.Min(count, text.Length);
+                    }
+
+                    stack.Push(text);
+                    text = text.Substring(0, text.Length - count);
+                }
+                else if (currentCommand == "3")
+                {
+                    var index = int.Parse(input[1]);
+
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (currentCommand == "4")
+                {
+                    text = stack.Pop();
+                }
+            }
+        }
+    }
+}

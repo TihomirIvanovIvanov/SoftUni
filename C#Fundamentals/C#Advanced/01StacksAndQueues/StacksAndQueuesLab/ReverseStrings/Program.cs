6fd0dc2d37fd78f75afd279c893d66a5e676dfac
tihomir <tihomir_2005@abv.bg>
2020-08-13 13:41:14 +0300

    Recap add lecture, lab and exercises description and 1 task from StacksAndQueues lab

---
+++ C#Fundamentals/C#Advanced/01StacksAndQueues/StacksAndQueuesLab/ReverseStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            Console.WriteLine(string.Join(string.Empty, stack));
+        }
+    }
+}

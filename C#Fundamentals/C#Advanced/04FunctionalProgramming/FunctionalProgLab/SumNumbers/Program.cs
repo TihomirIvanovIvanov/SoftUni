0563eb843ad9da187ba22648ae86e96c524fbf19
tihomir <tihomir_2005@abv.bg>
2020-08-14 16:55:15 +0300

    Recap add lecture, lab and exercises description and 1 and 2 task from lab

---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgLab/SumNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine();
+
+            Func<string, int> count = x =>
+                x.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .Count();
+
+            Func<string, int> sum = x =>
+                x.Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Sum();
+
+            Console.WriteLine(count(numbers));
+            Console.WriteLine(sum(numbers));
+        }
+    }
+}

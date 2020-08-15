ffdda9e71cf3dea75760f43e0cf3de93a49c8ad7
tihomir <tihomir_2005@abv.bg>
2020-08-15 11:18:03 +0300

    Recap add 4 and 5 task from Functional Programming exercises

---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgExer/AppliedArithmetics/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string line;
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                switch (line)
+                {
+                    case "add": Add(numbers); break;
+                    case "subtract": Subtract(numbers); break;
+                    case "multiply": Multiply(numbers); break;
+                    case "print": Print(numbers); break;
+                }
+            }
+        }
+
+        private static void Print(List<int> numbers)
+        {
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static void Multiply(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] *= 2;
+            }
+        }
+
+        private static void Subtract(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i]--;
+            }
+        }
+
+        private static void Add(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i]++;
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgExer/FindEvensOrOdds/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var left = int.Parse(input[0]);
+            var right = int.Parse(input[1]);
+
+            var parameter = Console.ReadLine();
+            var numbers = new List<int>();
+
+            var checkFunc = CreateFuntion(parameter);
+
+            for (int i = left; i <= right; i++)
+            {
+                if (checkFunc(i))
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static Predicate<int> CreateFuntion(string parameter)
+        {
+            if (parameter == "even")
+            {
+                return number => number % 2 == 0;
+            }
+
+            if (parameter == "odd")
+            {
+                return number => number % 2 != 0;
+            }
+
+            throw new Exception("Invalid parameter!");
+        }
+    }
+}

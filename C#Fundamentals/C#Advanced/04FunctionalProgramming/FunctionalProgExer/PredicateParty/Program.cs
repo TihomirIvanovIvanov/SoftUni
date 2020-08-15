7278beb8dbea17a8f2ddf03126f3d34e1a70e7b7
tihomir <tihomir_2005@abv.bg>
2020-08-15 19:49:13 +0300

    Recap add 10 and 11 task from Functional Programming exercises

---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgExer/PredicateParty/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string line;
+            while ((line = Console.ReadLine()) != "Party!")
+            {
+                var tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var command = tokens[0];
+                var criteria = tokens[1];
+                var parameter = tokens[2];
+
+                Predicate<string> startsWith = str => str.StartsWith(parameter);
+                Predicate<string> endsWith = str => str.EndsWith(parameter);
+                Predicate<string> lengthIs = str => str.Length == int.Parse(parameter);
+
+                if (command == "Double")
+                {
+                    switch (criteria)
+                    {
+                        case "StartsWith":
+                            {
+                                for (int i = 0; i < names.Count; i++)
+                                {
+                                    if (startsWith(names[i]))
+                                    {
+                                        names.Insert(i, names[i++]);
+                                    }
+                                }
+                                break;
+                            }
+                        case "EndsWith":
+                            {
+                                for (int i = 0; i < names.Count; i++)
+                                {
+                                    if (endsWith(names[i]))
+                                    {
+                                        names.Insert(i, names[i++]);
+                                    }
+                                }
+                                break;
+                            }
+                        case "Length":
+                            {
+                                for (int i = 0; i < names.Count; i++)
+                                {
+                                    if (lengthIs(names[i]))
+                                    {
+                                        names.Insert(i, names[i++]);
+                                    }
+                                }
+                                break;
+                            }
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    switch (criteria)
+                    {
+                        case "StartsWith": names.RemoveAll(startsWith); break;
+                        case "EndsWith": names.RemoveAll(endsWith); break;
+                        case "Length": names.RemoveAll(lengthIs); break;
+                    }
+                }
+            }
+
+            Console.WriteLine(names.Count == 0 ?
+                              "Nobody is going to the party!" :
+                              $"{string.Join(", ", names)} are going to the party!");
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgExer/ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThePartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var startsWithCollection = new HashSet<string>();
+            var endsWithCollection = new HashSet<string>();
+            var lengthCollection = new HashSet<int>();
+            var containsCollection = new HashSet<string>();
+
+            string line;
+            while ((line = Console.ReadLine()) != "Print")
+            {
+                var tokens = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                var command = tokens[0];
+                var filter = tokens[1];
+                var parameter = tokens[2];
+
+                if (command == "Add filter")
+                {
+                    switch (filter)
+                    {
+                        case "Starts with": startsWithCollection.Add(parameter); break;
+                        case "Ends with": endsWithCollection.Add(parameter); break;
+                        case "Length": lengthCollection.Add(int.Parse(parameter)); break;
+                        case "Contains": containsCollection.Add(parameter); break;
+                    }
+                }
+                else if (command == "Remove filter")
+                {
+                    switch (filter)
+                    {
+                        case "Starts with": startsWithCollection.Remove(parameter); break;
+                        case "Ends with": endsWithCollection.Remove(parameter); break;
+                        case "Length": lengthCollection.Remove(int.Parse(parameter)); break;
+                        case "Contains": containsCollection.Remove(parameter); break;
+                    }
+                }
+            }
+
+            startsWithCollection.ToList()
+                .ForEach(x =>
+                {
+                    Predicate<string> startsWith = str => str.StartsWith(x);
+                    names = names.Where(name => !startsWith(name)).ToList();
+                });
+            endsWithCollection.ToList()
+                .ForEach(x =>
+                {
+                    Predicate<string> endsWith = str => str.EndsWith(x);
+                    names = names.Where(name => !endsWith(name)).ToList();
+                });
+            lengthCollection.ToList()
+               .ForEach(x =>
+               {
+                   bool lengthIs(string str) => str.Length == x;
+                   names = names.Where(name => !lengthIs(name)).ToList();
+               });
+            containsCollection.ToList()
+               .ForEach(x =>
+               {
+                   Predicate<string> contais = str => str.Contains(x);
+                   names = names.Where(name => !contais(name)).ToList();
+               });
+            Console.WriteLine(string.Join(' ', names));
+        }
+    }
+}

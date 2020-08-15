c62773dab6e061c3bdedd657a05e3083ad119f33
tihomir <tihomir_2005@abv.bg>
2020-08-15 11:29:48 +0300

    Recap add 6 and 7 task from Functional Programming exercises

---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgExer/PredicateForNames/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Predicate<string> checkName = name => name.Length <= length;
+            var result = names.Where(name => checkName(name));
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
---
+++ C#Fundamentals/C#Advanced/04FunctionalProgramming/FunctionalProgExer/ReverseAndExclude/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var n = int.Parse(Console.ReadLine());
+
+            numbers = Remove(numbers, n);
+            numbers = Reverse(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static List<int> Reverse(List<int> numbers)
+        {
+            var reversed = new Stack<int>();
+            foreach (var number in numbers)
+            {
+                reversed.Push(number);
+            }
+            return reversed.ToList();
+        }
+
+        private static List<int> Remove(List<int> numbers, int n)
+        {
+            Predicate<int> removeFunc = number => number % n != 0;
+            return numbers.Where(x => removeFunc(x)).ToList();
+        }
+    }
+}

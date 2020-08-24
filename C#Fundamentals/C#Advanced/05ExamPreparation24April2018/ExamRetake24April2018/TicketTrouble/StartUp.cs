fd72609a37c2da4b72bef38c38240a9298504655
tihomir <tihomir_2005@abv.bg>
2020-08-24 22:40:10 +0300

    Recap add description and 3 task Ticket Trouble from C# Advanced Exam Retake - 24 April 2018

---
+++ C#Fundamentals/C#Advanced/05ExamPreparation24April2018/ExamRetake24April2018/TicketTrouble/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TicketTrouble
+{
+    public class StartUp
+    {
+        private static string targetCountry;
+
+        private static string targetCity;
+
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            targetCountry = input[0];
+            targetCity = input[1];
+
+            var pattern = new Regex(@"((?<bracket>{)|\[)[^{[]*?(?(bracket)\[|{)(?<country>[A-Z]{3}) (?<city>[A-Z]{2})(?(bracket)\]|})[^[{]*?(?(bracket)\[|{)(?<letter>[A-Z])(?<number>[0-9]{2}|[0-9]{1})(?(bracket)\]|})[^]}]*?(?(bracket)}|\])");
+
+            var allTickets = Console.ReadLine();
+
+            var matches = pattern.Matches(allTickets);
+            var validTickets = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                var country = match.Groups["country"].Value;
+                var city = match.Groups["city"].Value;
+                var letter = match.Groups["letter"].Value;
+                var number = match.Groups["number"].Value;
+
+                if (country == targetCountry && city == targetCity)
+                {
+                    if (validTickets.Contains(number))
+                    {
+                        var index = validTickets.IndexOf(number);
+                        Print(validTickets[index - 1] + validTickets[index], letter + number);
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        validTickets.Add(letter);
+                        validTickets.Add(number);
+                    }
+                }
+            }
+            Print(validTickets[0] + validTickets[1], validTickets[2] + validTickets[3]);
+        }
+
+        private static void Print(string firstSeat, string secondSeat)
+        {
+            Console.WriteLine($"You are traveling to {targetCountry} {targetCity} on seats {firstSeat} and {secondSeat}.");
+        }
+    }
+}

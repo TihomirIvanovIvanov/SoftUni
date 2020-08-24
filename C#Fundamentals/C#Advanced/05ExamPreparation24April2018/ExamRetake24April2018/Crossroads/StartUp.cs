7689ea1da7fc3824dfef2168591baddd4cc9b64f
tihomir <tihomir_2005@abv.bg>
2020-08-24 21:49:19 +0300

    Recap add description and 1 task Crossroads from C# Advanced Exam Retake - 24 April 2018

---
+++ C#Fundamentals/C#Advanced/05ExamPreparation24April2018/ExamRetake24April2018/Crossroads/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crossroads
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var greenTime = int.Parse(Console.ReadLine());
+            var freeWindow = int.Parse(Console.ReadLine());
+
+            var cars = new Queue<string>();
+            var totalCars = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "green")
+                {
+                    GreenLightOn(cars, greenTime, freeWindow);
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                    totalCars++;
+                }
+            }
+
+            Console.WriteLine($"Everyone is safe.{Environment.NewLine}" +
+                $"{totalCars - cars.Count} total cars passed the crossroads");
+        }
+
+        private static void GreenLightOn(Queue<string> cars, int greenTime, int freeWindow)
+        {
+            var car = string.Empty;
+
+            while (cars.Count > 0 && greenTime > 0)
+            {
+                car = cars.Dequeue();
+                if (car.Length <= greenTime)
+                {
+                    greenTime -= car.Length;
+                }
+                else
+                {
+                    var partsInside = car.Substring(greenTime);
+                    IsCrash(partsInside, freeWindow, car);
+                    break;
+                }
+            }
+        }
+
+        private static void IsCrash(string partsInside, int freeWindow, string car)
+        {
+            if (partsInside.Length > freeWindow)
+            {
+                Console.WriteLine($"A crash happend!{Environment.NewLine}{car} was hit at {partsInside[freeWindow]}.");
+                Environment.Exit(0);
+            }
+        }
+    }
+}

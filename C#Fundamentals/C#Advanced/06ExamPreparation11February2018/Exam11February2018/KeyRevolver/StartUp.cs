4e4e6f8aa4ccda8a1d06471592d714705c5220e5
tihomir <tihomir_2005@abv.bg>
2020-08-25 18:38:47 +0300

    Recap add description and 1 task Key Revolver from C# Advanced Exam - 11 February 2018

---
+++ C#Fundamentals/C#Advanced/06ExamPreparation11February2018/Exam11February2018/KeyRevolver/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyRevolver
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var pricePerBullet = int.Parse(Console.ReadLine());
+            var barrelSize = int.Parse(Console.ReadLine());
+
+            var bulletsArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var bullets = new Stack<int>(bulletsArr);
+
+            var lockArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var locks = new Queue<int>(lockArr);
+
+            var value = int.Parse(Console.ReadLine());
+            var counter = 0;
+
+            while (locks.Count > 0 && bullets.Count > 0)
+            {
+                var bullet = bullets.Pop();
+
+                if (bullet <= locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                counter++;
+
+                if (counter % barrelSize == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+            if (locks.Count == 0)
+            {
+                var initialBulletsCount = bulletsArr.Length;
+                var spentForBullets = counter * pricePerBullet;
+                var result = value - spentForBullets;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${result}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+        }
+    }
+}

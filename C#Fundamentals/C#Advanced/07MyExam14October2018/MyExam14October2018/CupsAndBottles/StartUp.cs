0766b57934f6265e5d635f904bcde9e6e5361839
tihomir <tihomir_2005@abv.bg>
2020-08-26 14:42:55 +0300

    Recap add 4 task Cups and Bottles from C# Advanced Exam - 14 October 2018

---
+++ C#Fundamentals/C#Advanced/07MyExam14October2018/MyExam14October2018/CupsAndBottles/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace CupsAndBottles
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var cups = new Queue(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            var bottles = new Stack(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            var wastedLiters = 0;
+            var cup = 1;
+            var bottle = 1;
+            var first = true;
+
+            while (cups.Count > 0 && bottles.Count > 0)
+            {
+                if (cup <= 0 || first)
+                {
+                    cup = int.Parse(cups.Peek().ToString());
+                }
+
+                bottle = int.Parse(bottles.Peek().ToString());
+                cup -= bottle;
+
+                if (cup <= 0)
+                {
+                    wastedLiters += Math.Abs(cup);
+                    cups.Dequeue();
+                    bottles.Pop();
+                }
+                else
+                {
+                    bottles.Pop();
+
+                    while (cup > 0)
+                    {
+                        bottle = int.Parse(bottles.Pop().ToString());
+                        cup -= bottle;
+
+                        if (cup <= 0)
+                        {
+                            wastedLiters += Math.Abs(cup);
+                            cups.Dequeue();
+                            break;
+                        }
+                    }
+                }
+
+                first = false;
+            }
+
+            if (cups.Count > 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(' ', cups.ToArray())}");
+            }
+            else if (bottles.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(' ', bottles.ToArray())}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {wastedLiters}");
+        }
+    }
+}

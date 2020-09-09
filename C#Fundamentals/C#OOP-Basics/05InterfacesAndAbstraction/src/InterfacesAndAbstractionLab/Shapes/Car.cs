6c71ce6be0750567ea9060bbf15878d48bbda15f
tihomir <tihomir_2005@abv.bg>
2020-09-09 11:58:28 +0300

    Recap add 1-2 task from Interfaces and Abstraction - Lab /2 task is in 1 task project/

---
+++ C#Fundamentals/C#OOP-Basics/05InterfacesAndAbstraction/src/InterfacesAndAbstractionLab/Shapes/Car.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public abstract class Car : ICar
+    {
+        protected Car(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public virtual string GetCarInfo()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model}";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(this.GetCarInfo())
+                .AppendLine(this.Start())
+                .AppendLine(this.Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#Fundamentals/C#OOP-Basics/05InterfacesAndAbstraction/src/InterfacesAndAbstractionLab/Shapes/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        //public static void Main()
+        //{
+        //    var radius = int.Parse(Console.ReadLine());
+        //    IDrawable circle = new Circle(radius);
+
+        //    var width = int.Parse(Console.ReadLine());
+        //    var height = int.Parse(Console.ReadLine());
+        //    IDrawable rect = new Rectangle(width, height);
+
+        //    circle.Draw();
+        //    rect.Draw();
+        //}
+        public static void Main()
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+        }
+    }
+}

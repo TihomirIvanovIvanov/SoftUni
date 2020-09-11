990d0cab92058ae74a235dd5baa75e6d86641770
tihomir <tihomir_2005@abv.bg>
2020-09-11 16:25:51 +0300

    Recap add 1-3 task from Polymorphism - Lab

---
+++ C#Fundamentals/C#OOP-Basics/06Polymorphism/src/PolymorphismLab/Shapes/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get => this.radius;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentNullException(nameof(this.Radius), "Cannot be zero or negative!");
+                }
+                this.radius = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(this.Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ C#Fundamentals/C#OOP-Basics/06Polymorphism/src/PolymorphismLab/Shapes/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get => this.width;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentNullException(nameof(this.Width), "Cannot be zero or negative!");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentNullException(nameof(this.Height), "Cannot be zero or negative!");
+                }
+                this.height = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.Width + 2 * this.Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
